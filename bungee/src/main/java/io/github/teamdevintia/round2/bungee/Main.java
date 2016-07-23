b79ae1244de832de1bf3ceb7b18e8e5366fa1581
Shad0wCore <shad0wcoretv@gmail.com>
2016-07-23 11:18:21 +0200

    Initial Commit of BungeeCord Module / Changes in .gitignore

--- bungee/src/main/java/io/github/teamdevintia/round2/bungee/Main.java
+++ bungee/src/main/java/io/github/teamdevintia/round2/bungee/Main.java
@@ -2,9 +2,10 @@ package io.github.teamdevintia.round2.bungee;
 
 /**
  * @author Shad0wCore
+ *         <p>
+ *         Represents the main class for the stuff working with BungeeCord
  */
 public class Main {
-
 
 
 }
