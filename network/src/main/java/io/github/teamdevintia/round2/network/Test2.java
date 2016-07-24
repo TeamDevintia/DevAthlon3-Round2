d5b76bfa52280a91e1162423eec137ee6f5ce815
Shad0wCore <shad0wcoretv@gmail.com>
2016-07-24 23:10:55 +0200

    Changed something

--- network/src/main/java/io/github/teamdevintia/round2/network/Test2.java
+++ network/src/main/java/io/github/teamdevintia/round2/network/Test2.java
@@ -1,10 +1,8 @@
 package io.github.teamdevintia.round2.network;
 
 import io.github.teamdevintia.round2.network.internal.EventBus;
-import io.github.teamdevintia.round2.network.pipeline.StreamHandler;
 import io.github.teamdevintia.round2.network.internal.handlers.ClientNetHandler;
-import io.github.teamdevintia.round2.network.packet.ComponentPacket;
-import io.github.teamdevintia.round2.network.pipeline.MessageSerializer;
+import io.github.teamdevintia.round2.network.pipeline.StreamHandler;
 
 /**
  * @author Shad0wCore
@@ -17,11 +15,7 @@ public class Test2 {
         clientNetHandler.establishConnection("127.0.0.1", 8000, new Callback<StreamHandler>() {
             @Override
             public void trigger(StreamHandler streamHandler) {
-                MessageSerializer messageSerializer = new MessageSerializer("information");
-                messageSerializer.addProperty("numPlayers", 123). addProperty("maxPlayers", 234);
-                messageSerializer.addProperty("currentRam", 1024). addProperty("maxRam", 3056);
-                messageSerializer.addProperty("tps", 20). addProperty("motd", "A Minecraft Server");
-                streamHandler.handlePacket(new ComponentPacket(EnumPacketDirection.GLOBAL, messageSerializer.serialize()));
+
             }
         });
     }
