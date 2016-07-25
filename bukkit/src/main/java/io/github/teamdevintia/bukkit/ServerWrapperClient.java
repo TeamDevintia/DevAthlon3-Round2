03fe4b526f32f8fc5780a3c5c740d91cea648f13
MiniDigger <mini.digger@live.de>
2016-07-25 08:05:32 +0200

    add missing import

--- bukkit/src/main/java/io/github/teamdevintia/bukkit/ServerWrapperClient.java
+++ bukkit/src/main/java/io/github/teamdevintia/bukkit/ServerWrapperClient.java
@@ -9,6 +9,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
